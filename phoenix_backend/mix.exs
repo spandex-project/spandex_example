6a4a2f71f993918db908e8c5d5ce358c1a1784ea
Greg Mefford <greg@gregmefford.com>
2018-09-30 14:49:38 -0400

    alphabetize phoenix_backend deps

--- phoenix_backend/mix.exs
+++ phoenix_backend/mix.exs
@@ -33,12 +33,12 @@ defmodule PhoenixBackend.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
+      {:cowboy, "~> 1.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:phoenix, "~> 1.3.4"},
+      {:phoenix_ecto, "~> 3.2"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"},
     ]
   end
 

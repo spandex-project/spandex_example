4105f5e5f37f80ba6c371d0ad82e115b1627241c
Greg Mefford <greg@gregmefford.com>
2018-10-10 14:56:59 -0400

    Add simple token auth to backend

--- phoenix_backend/lib/phoenix_backend_web/router.ex
+++ phoenix_backend/lib/phoenix_backend_web/router.ex
@@ -2,6 +2,7 @@ defmodule PhoenixBackendWeb.Router do
   use PhoenixBackendWeb, :router
 
   pipeline :api do
+    plug :ensure_auth
     plug :accepts, ["json"]
   end
 
@@ -13,4 +14,43 @@ defmodule PhoenixBackendWeb.Router do
 
     get "/users_n_plus_1", UserController, :index_n_plus_1
   end
+
+  defp ensure_auth(conn, _opts) do
+    token =
+      conn
+      |> get_req_header("authorization")
+      |> List.first()
+      |> extract_token()
+
+    expected_token = System.get_env("AUTH_TOKEN")
+
+    case token do
+      nil ->
+        conn
+        |> send_resp(401, ~s|{"errors":"Authorization: Bearer <token> header required"}|)
+        |> halt()
+
+      ^expected_token ->
+        conn
+
+      _ ->
+        conn
+        |> send_resp(403, ~s|{"errors":"Incorrect authorization token"}|)
+        |> halt()
+    end
+  end
+
+  defp extract_token(nil), do: nil
+
+  defp extract_token(header) do
+    header
+    |> String.split(" ", parts: 2)
+    |> case do
+      [type, token] -> extract_token(String.downcase(type), token)
+      _ -> nil
+    end
+  end
+
+  defp extract_token("bearer", token), do: token
+  defp extract_token(_, _), do: nil
 end

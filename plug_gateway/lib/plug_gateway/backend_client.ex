48c2f388247f19c9cb40fb18c9eb311e0f2d9b08
Greg Mefford <greg@gregmefford.com>
2018-10-10 15:10:03 -0400

    Call into backend from gateway

---
+++ plug_gateway/lib/plug_gateway/backend_client.ex
@@ -0,0 +1,15 @@
+defmodule PlugGateway.BackendClient do
+
+  def get(url, opts \\ []) do
+    headers = [{"authorization", "Bearer #{token()}"}]
+
+    url
+    |> HTTPoison.get(headers)
+    |> case do
+      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} -> {:ok, status_code, body}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp token, do: System.get_env("BACKEND_AUTH_TOKEN")
+end
--- plug_gateway/lib/plug_gateway/router.ex
+++ plug_gateway/lib/plug_gateway/router.ex
@@ -1,6 +1,8 @@
 defmodule PlugGateway.Router do
   use Plug.Router
 
+  alias PlugGateway.BackendClient
+
   plug :match
   plug Plug.Parsers, parsers: [:urlencoded, :json],
     pass: ["*/*"],
@@ -19,7 +21,27 @@ defmodule PlugGateway.Router do
     end
   end
 
+  get "/users" do
+    response =
+      BackendClient.get(backend_api_endpoint() <> "/users")
+      |> case do
+        {:ok, status_code, body} -> send_resp(conn, status_code, body)
+        {:error, reason} -> send_resp(conn, 502, ~s|{"errors":"#{reason}"}|)
+      end
+  end
+
+  get "/users_n_plus_1" do
+    response =
+      BackendClient.get(backend_api_endpoint() <> "/users_n_plus_1")
+      |> case do
+        {:ok, status_code, body} -> send_resp(conn, status_code, body)
+        {:error, reason} -> send_resp(conn, 502, ~s|{"errors":"#{reason}"}|)
+      end
+  end
+
   match _ do
     send_resp(conn, 404, "404: Not Found")
   end
+
+  defp backend_api_endpoint, do: System.get_env("BACKEND_API_URL")
 end
